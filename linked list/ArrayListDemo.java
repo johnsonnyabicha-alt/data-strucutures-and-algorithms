e1e51f23a39847d04f174a5f9c40fcd5145cd3cc
Johnson Nyabicha <johnsonnyabicha@gmail.com>
2026-08-23 18:46:53 +0300

    Remove ArrayListDemo.java file

--- linked list/ArrayListDemo.java
+++ linked list/ArrayListDemo.java
@@ -1,15 +1,1 @@
-import java.util.ArrayList; // Required import 
 
-public class ArrayListDemo {
-    public static void main(String[] args) {
-        ArrayList<String> devices = new ArrayList<>();
-        
-        // Dynamically adding objects [cite: 401]
-        devices.add("Phone");
-        devices.add("Laptop");
-        
-        // Reading elements [cite: 406]
-        System.out.println("First device: " + devices.get(0));
-        System.out.println("Total items: " + devices.size()); 
-    }
-}
